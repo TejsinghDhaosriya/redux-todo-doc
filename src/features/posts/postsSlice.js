e28ca91e947270315cabd6d3ea6a24342ab40ff8
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2021-01-09 15:21:32 +0530

    Added Action,Reducer,Thunk,FakeServer'

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,150 @@
+
+
+
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  return response.posts
+})
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.posts.push(action.payload)
+      },
+      prepare(title, content, userId) {
+          return {
+            payload: {
+              id: nanoid(),
+              date: new Date().toISOString(),
+              title,
+              content,
+              user: userId,
+              reactions: {
+                thumbsUp: 0,
+                hooray: 0,
+                heart: 0,
+                rocket: 0,
+                eyes: 0,
+              },
+            },
+          
+      }}
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.posts.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    },
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload
+      const existingPost = state.posts.find(post => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
+    }
+  }
+})
+
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
+
+export default postsSlice.reducer
+
+export const selectAllPosts = state => state.posts.posts
+
+export const selectPostById = (state, postId) =>
+  state.posts.posts.find(post => post.id === postId)
+
+
+
+
+  //old 
+
+  
+  // const initialState = [
+  //   {
+  //     id: '1',
+  //     title: 'First Post!',
+  //     content: 'Hello!',
+  //     user: '0',
+  //     date: sub(new Date(), { minutes: 10 }).toISOString(),
+  //     reactions: {
+  //       thumbsUp: 0,
+  //       hooray: 0,
+  //       heart: 0,
+  //       rocket: 0,
+  //       eyes: 0,
+  //     },
+  //   },
+  //   {
+  //     id: '2',
+  //     title: 'Second Post',
+  //     content: 'More text',
+  //     user: '2',
+  //     date: sub(new Date(), { minutes: 5 }).toISOString(),
+  //     reactions: {
+  //       thumbsUp: 0,
+  //       hooray: 0,
+  //       heart: 0,
+  //       rocket: 0,
+  //       eyes: 0,
+  //     },
+  //   },
+  // ]
+  
+  // const postsSlice = createSlice({
+  //   name: 'posts',
+  //   initialState,
+  //   reducers: {
+  //     postAdded: {
+  //       reducer(state, action) {
+  //         state.push(action.payload)
+  //       },
+  //       prepare(title, content, userId) {
+  //         return {
+  //           payload: {
+  //             id: nanoid(),
+  //             date: new Date().toISOString(),
+  //             title,
+  //             content,
+  //             user: userId,
+  //             reactions: {
+  //               thumbsUp: 0,
+  //               hooray: 0,
+  //               heart: 0,
+  //               rocket: 0,
+  //               eyes: 0,
+  //             },
+  //           },
+  //         }
+  //       },
+  //     },
+  //     reactionAdded(state, action) {
+  //       const { postId, reaction } = action.payload
+  //       const existingPost = state.find((post) => post.id === postId)
+  //       if (existingPost) {
+  //         existingPost.reactions[reaction]++
+  //       }
+  //     },
+  //     postUpdated(state, action) {
+  //       const { id, title, content } = action.payload
+  //       const existingPost = state.find((post) => post.id === id)
+  //       if (existingPost) {
+  //         existingPost.title = title
+  //         existingPost.content = content
+  //       }
+  //     },
+  //   },
+  // })
